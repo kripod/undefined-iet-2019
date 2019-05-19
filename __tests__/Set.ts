ff6d647358da7e54b0554fd8dacfeab885d4ca01
akosfi <fiakos1997@sch.bme.hu>
2019-05-19 21:30:37 +0200

    fixed travis errors

--- __tests__/Set.ts
+++ __tests__/Set.ts
@@ -325,42 +325,42 @@ describe('Set', () => {
 
   it('can find the value defined by predicate', () => {
     const set = Set([1, 2, 3, 4, 5]);
-    expect(set.find(x => x == 2)).toBe(2);
+    expect(set.find(x => x === 2)).toBe(2);
   });
 
   it('can find the last value defined by predicate', () => {
     const set = Set([1, 2, 3, 4, 5]);
-    expect(set.findLast(x => x % 2 == 0)).toBe(4);
+    expect(set.findLast(x => x % 2 === 0)).toBe(4);
   });
 
   it('can find the entry defined by predicate', () => {
     const set = Set([1, 2, 3, 4, 5]);
-    expect(set.findEntry(x => x == 2)).toEqual([2, 2]);
+    expect(set.findEntry(x => x === 2)).toEqual([2, 2]);
   });
 
   it('can find the last entry defined by predicate', () => {
     const set = Set([1, 2, 3, 4, 5]);
-    expect(set.findLastEntry(x => x % 2 == 0)).toEqual([4, 4]);
+    expect(set.findLastEntry(x => x % 2 === 0)).toEqual([4, 4]);
   });
 
   it('can find the key defined by predicate', () => {
     const set = Set([1, 2, 3, 4, 5]);
-    expect(set.findKey(x => x == 2)).toBe(2);
+    expect(set.findKey(x => x === 2)).toBe(2);
   });
 
   it('can find the last key defined by predicate', () => {
     const set = Set([1, 2, 3, 4, 5]);
-    expect(set.findLastKey(x => x % 2 == 0)).toBe(4);
+    expect(set.findLastKey(x => x % 2 === 0)).toBe(4);
   });
 
   it('can find the key associated with a value', () => {
     const set = Set([1, 2, 3, 4, 5]);
-    expect(set.keyOf(x => x == 2)).toBe(undefined); //no keys, just values
+    expect(set.keyOf(x => x === 2)).toBe(undefined);
   });
 
   it('can find the last key associated with a value', () => {
     const set = Set([1, 2, 3, 4, 5]);
-    expect(set.lastKeyOf(x => x == 2)).toBe(undefined); //no keys, just values
+    expect(set.lastKeyOf(x => x === 2)).toBe(undefined);
   });
 
   it('can find the max value', () => {
